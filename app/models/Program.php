095c7b3b53f5cd50b7dca9515942b48127795891
Fredrik Wollsén <fredrik@neam.se>
2014-01-28 21:22:29 +0100

    Accepting ISO RFC3339 formatted dates for date_utc as well as MySQL common datetime formats

--- app/models/Program.php
+++ app/models/Program.php
@@ -34,12 +34,55 @@ class Program extends BaseProgram
     public function rules()
     {
         return array_merge(
-            parent::rules()
-        /* , array(
-          array('column1, column2', 'rule1'),
-          array('column3', 'rule2'),
-          ) */
+            parent::rules(),
+            array(
+                array('date_utc', 'validateDateUtc'),
+            )
         );
+    }
+
+    public function validateDateUtc()
+    {
+        // Store date in UTC
+        try {
+            $date_utc = $this->parseDateUtc($this->date_utc);
+        } catch (Exception $e) {
+            $this->addError('date_utc', Yii::t('app', 'Could not parse date'));
+        }
+        if (!($date_utc instanceof DateTime)) {
+            $this->addError('date_utc', Yii::t('app', 'Parsed date not valid')); // TODO: Change to a more descriptive error message
+        }
+    }
+
+    public function beforeSave()
+    {
+
+        // Store date in UTC in a format that MySQL plays along with
+        try {
+            $date_utc = $this->parseDateUtc($this->date_utc);
+            $date_utc->setTimezone(new DateTimeZone('UTC'));
+            $this->date_utc = $date_utc->format("Y-m-d H:i:s");
+        } catch (Exception $e) {
+            $this->date_utc = null;
+        }
+
+    }
+
+    protected function parseDateUtc($date_utc)
+    {
+        // Recognized formats
+        $formats = array(
+            DateTime::RFC3339,
+            "Y-m-d H:i:s",
+            "Y-m-d H:i",
+        );
+        foreach ($formats as $format) {
+            $parsed = DateTime::createFromFormat($format, $date_utc, new DateTimeZone('UTC')); // TODO: Check how UTC works as argument when input includes timezone offset
+            if (!empty($parsed)) {
+                break;
+            }
+        }
+        return $parsed;
     }
 
     public function search()
