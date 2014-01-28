8a06a25b562560d6339f63e78262fb30dd28b4d7
Fredrik Wollsén <fredrik@neam.se>
2014-01-28 20:37:03 +0100

    Current "local" configuration files

---
+++ app/config/main-local.php
@@ -0,0 +1,20 @@
+<?php
+
+// Use this file as main-local.php to override settings only on your local machine
+
+return array(
+    'params' => array(
+        'env' => 'development'
+    ),
+    'components' => array(
+        // MySQL
+        /*'db'            => array(
+            'tablePrefix'      => '',
+            'connectionString' => 'mysql:host=localhost;dbname=p3',
+            'emulatePrepare' => true,
+            'username' => 'test',
+            'password' => 'test',
+            'charset' => 'utf8',
+        ),*/
+    )
+);
